5c64ae07c0f9052c968c1753a276656d19240c4a
Michael Hamlett <mikeyevander@Michaels-MacBook-Pro-4.local>
2017-08-03 10:51:08 -0700

    config

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://<dbuser>:<dbpassword>@ds111622.mlab.com:11622/serve',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'serve',
   masterKey: process.env.MASTER_KEY || 'abcdefg', //Add your master key here. Keep it secret!
