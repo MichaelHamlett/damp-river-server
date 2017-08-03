81c57d04ddc4d95426079b9017e2f26cd7e1d31f
Michael Hamlett <mhamlett@hmc.edu>
2017-08-03 15:06:38 -0700

    push not

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,17 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.push.send({
+  where: {
+    "deviceType": "ios"
+  },
+  "data" : {
+    "alert": "Breaking News!",
+     "sound": "default"
+  }
+}, { useMasterKey: true}).then(() => {
+  console.log('Push ok');
+}, (e) => {
+  console.log('Push error', e);
+});
